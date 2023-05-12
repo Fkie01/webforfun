e55bb5de0fa6be456c009d8b5db09d8bd924ec5c
Fluke <48594334+Fkie01@users.noreply.github.com>
2023-05-12 15:52:29 -0300

    intial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('helloworld')
+function myfunction(){
+    var x = document.getElementById("input").value ;
+    document.getElementById("demo").innerHTML = "Your are thinking about " + x;
+
+}
